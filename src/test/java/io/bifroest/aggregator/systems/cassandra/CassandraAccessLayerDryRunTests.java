4de96e46f73980fbee049b7912ca9c5de38fc75e
Harald Kraemer <tetha42@gmail.com>
2016-02-01 20:15:34 +0100

    Added test: calling open + close in dry run opens + closes the db

--- src/test/java/io/bifroest/aggregator/systems/cassandra/CassandraAccessLayerDryRunTests.java
+++ src/test/java/io/bifroest/aggregator/systems/cassandra/CassandraAccessLayerDryRunTests.java
@@ -1,9 +1,11 @@
 package io.bifroest.aggregator.systems.cassandra;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.time.Duration;
 
+import com.datastax.driver.core.Session;
 import io.bifroest.retentions.RetentionConfiguration;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +18,9 @@ public class CassandraAccessLayerDryRunTests {
 
     @Mock
     public RetentionConfiguration retentionConfiguration;
+
+    @Mock
+    public Session session;
 
     public boolean doDryRun = true;
 
@@ -34,5 +39,16 @@ public class CassandraAccessLayerDryRunTests {
         subject.open();
 
         verify(cluster).open();
+    }
+
+    @Test
+    public void closeClosesSessionAndCluster() {
+        when(cluster.open()).thenReturn(session);
+
+        subject.open();
+        subject.close();
+
+        verify(cluster).close();
+        verify(session).close();
     }
 }
